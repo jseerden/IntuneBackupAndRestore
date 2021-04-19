429e5067e2bc406f4b7861bcb78bd0105d70fac6
John Seerden <john@seerden.it>
2021-04-19 14:58:12 +0200

    Add Settion Catalog support

---
+++ IntuneBackupAndRestore/Public/Invoke-IntuneBackupConfigurationPolicy.ps1
@@ -0,0 +1,54 @@
+function Invoke-IntuneBackupConfigurationPolicy {
+    <#
+    .SYNOPSIS
+    Backup Intune Settings Catalog Policies
+    
+    .DESCRIPTION
+    Backup Intune Settings Catalog Policies as JSON files per Settings Catalog Policy to the specified Path.
+    
+    .PARAMETER Path
+    Path to store backup files
+    
+    .EXAMPLE
+    Invoke-IntuneBackupConfigurationPolicy -Path "C:\temp"
+    #>
+    
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$Path,
+
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("v1.0", "Beta")]
+        [string]$ApiVersion = "Beta"
+    )
+
+    # Set the Microsoft Graph API endpoint
+    if (-not ((Get-MSGraphEnvironment).SchemaVersion -eq $apiVersion)) {
+        Update-MSGraphEnvironment -SchemaVersion $apiVersion -Quiet
+        Connect-MSGraph -ForceNonInteractive -Quiet
+    }
+
+    # Create folder if not exists
+    if (-not (Test-Path "$Path\Settings Catalog")) {
+        $null = New-Item -Path "$Path\Settings Catalog" -ItemType Directory
+    }
+
+    # Get all Setting Catalogs Policies
+    $configurationPolicies = Invoke-MSGraphRequest -HttpMethod GET -Url "deviceManagement/configurationPolicies" | Get-MSGraphAllPages
+
+    foreach ($configurationPolicy in $configurationPolicies) {
+        $configurationPolicy | Add-Member -MemberType NoteProperty -Name 'settings' -Value @() -Force
+        $settings = Invoke-MSGraphRequest -HttpMethod GET -Url "deviceManagement/configurationPolicies/$($configurationPolicy.id)/settings" | Get-MSGraphAllPages
+
+        if ($settings -isnot [System.Array]) {
+            $configurationPolicy.Settings = @($settings)
+        } else {
+            $configurationPolicy.Settings = $settings
+        }
+        
+        Write-Output "Backing Up - Settings Catalog Policy: $($configurationPolicy.name)"
+        $fileName = ($configurationPolicy.name).Split([IO.Path]::GetInvalidFileNameChars()) -join '_'
+        $configurationPolicy | ConvertTo-Json -Depth 100 | Out-File -LiteralPath "$path\Settings Catalog\$fileName.json"
+    }
+}
---
+++ IntuneBackupAndRestore/Public/Invoke-IntuneRestoreConfigurationPolicy.ps1
@@ -0,0 +1,51 @@
+function Invoke-IntuneRestoreConfigurationPolicy {
+    <#
+    .SYNOPSIS
+    Restore Intune Settings Catalog Policies
+    
+    .DESCRIPTION
+    Restore Intune Settings Catalog Policies from JSON files per Settings Catalog Policy from the specified Path.
+    
+    .PARAMETER Path
+    Root path where backup files are located, created with the Invoke-IntuneBackupConfigurationPolicy function
+    
+    .EXAMPLE
+    Invoke-IntuneRestoreConfigurationPolicy -Path "C:\temp"
+    #>
+    
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$Path,
+
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("v1.0", "Beta")]
+        [string]$ApiVersion = "Beta"
+    )
+
+    # Set the Microsoft Graph API endpoint
+    if (-not ((Get-MSGraphEnvironment).SchemaVersion -eq $apiVersion)) {
+        Update-MSGraphEnvironment -SchemaVersion $apiVersion -Quiet
+        Connect-MSGraph -ForceNonInteractive -Quiet
+    }
+
+    # Get all Settings Catalog Policies
+    $configurationPolicies = Get-ChildItem -Path "$Path\Settings Catalog" -File
+
+    foreach ($configurationPolicy in $configurationPolicies) {
+        $configurationPolicyContent = Get-Content -LiteralPath $configurationPolicy.FullName -Raw | ConvertFrom-Json
+        
+        # Remove properties that are not available for creating a new configuration
+        $requestBody = $configurationPolicyContent | Select-Object -Property * -ExcludeProperty id, createdDateTime, lastModifiedDateTime, settingCount, creationSource | ConvertTo-Json -Depth 100
+
+        # Restore the Settings Catalog Policy
+        try {
+            $null = Invoke-MSGraphRequest -HttpMethod POST -Content $requestBody.toString() -Url "deviceManagement/configurationPolicies" -ErrorAction Stop
+            Write-Output "$($configurationPolicy.FullName) - Successfully restored Settings Catalog Policy"
+        }
+        catch {
+            Write-Output "$($configurationPolicy.FullName) - Failed to restore Settings Catalog Policy"
+            Write-Error $_ -ErrorAction Continue
+        }
+    }
+}
